02377f847710c64a72391bc20e976450730c0397
ronin1375 <ronins1375@gmail.com>
2022-04-16 16:32:43 +0430

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import { React } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
     <AboutUs/>
     </div>
   );
