0fa81d3ea1c3ff908d7d9a51ac6723c17475aea5
ronin1375 <ronins1375@gmail.com>
2022-04-16 14:03:02 +0430

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,12 @@
 
 //Page Components
 import AboutSection from "../components/AboutSection";
-
+import ServicesSection from "../components/ServicesSection";
 const AboutUs = () =>{
-    return(
+    return( <>
 <AboutSection/>
-    )
+<ServicesSection/>
+</>);
 }
 
 export default AboutUs;
